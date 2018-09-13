cf4ffe6288628c5ce4f5195c7cd4d1c89f166df5
Oluwasanmi Aderibigbe <sanmiaderibigbe@outlook.com>
2018-09-13 08:42:29 +0100

    Refactored app to single activity, multiple fragment.

--- app/src/main/java/com/adetech/garedprintsreminder/MainActivity.kt
+++ app/src/main/java/com/adetech/garedprintsreminder/MainActivity.kt
@@ -1,27 +1,29 @@
 package com.adetech.garedprintsreminder
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import kotlinx.android.synthetic.main.activity_orders.*
-import kotlinx.android.synthetic.main.app_bar_orders.*
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
-class OrdersActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_orders)
+        setContentView(R.layout.activity_main)
+        initFragment(OrdersListFragment.newInstance())
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+//        fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
+//        }
 
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
@@ -63,5 +65,28 @@ class OrdersActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    /***
+     * @param
+     */
+    private fun replaceFragment(fragment: Fragment): Unit {
+
+        if (supportFragmentManager.findFragmentById(R.id.fragment_container)::class.simpleName == fragment::class.simpleName) {
+            Toast.makeText(this, fragment::class.java.simpleName, Toast.LENGTH_SHORT).show()
+        } else {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
+        }
+
+        //TODo rearrange  ui project.
+    }
+
+
+    private fun initFragment(fragment: Fragment): Unit {
+
+        if (supportFragmentManager.findFragmentById(R.id.fragment_container) == null) {
+            supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
+        }
+
     }
 }
